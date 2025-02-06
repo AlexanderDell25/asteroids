399bf7bd83d0e2f50e8389f22ff263c35691cd6e
AlexanderDell25 <alexanderdell@gmx.net>
2025-02-06 18:54:06 +0100

    C: Rotation

--- main.py
+++ main.py
@@ -16,16 +16,16 @@ def main():
     
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
-    draw_player = Player(x,y)
+    player = Player(x,y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
-        draw_player.draw(screen)
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
-        print(dt)
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
